2d325b192ad0d38c473a082e4b2b55bc34226b54
Tshegofatso Jan <mabatshegofatso842@gmail.com>
2023-02-16 21:04:51 -0800

    Use puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+ * the use puts function
+ * always return (0)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
