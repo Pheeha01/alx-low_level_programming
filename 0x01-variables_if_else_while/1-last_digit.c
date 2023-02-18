3c9f8b4be2e514c75324f95362540172e8d9c6bf
Tshegofatso Jan <mabatshegofatso842@gmail.com>
2023-02-18 06:47:11 -0800

    extracting the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,14 +12,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	char last_d;
+	int last_d;
 
 	last_d = n % 10;
-	if (last_d == 0)
-		printf("Last digit of %d is %c and is 0", n, last_d);
-	else if (last_d >= 6)
-		printf("Last digit of %d is %c and is greater than 5", n, last_d);
+	if (last_d > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
+	}
+	else if (last_d == 0)
+	{
+		printf("Last digit of %d is %d and is zero\n", n, last_d);
+	}
 	else
-		printf("Last digit of %d is %c and is less than 6 and not 0", n, last_d);
+	{
+		printf("Last digit of %d is %c and is less than 6 and not 0\n", n, last_d);
+	};
 	return (0);
 }
