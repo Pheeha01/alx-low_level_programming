ddc602b0a78db2e08097b3cd5ab1316099973fc2
Tshegofatso Jan <mabatshegofatso842@gmail.com>
2023-02-19 14:31:38 -0800

    Printing a combination of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - printing all possible combinations
+ * using Putchar, need to study it
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
