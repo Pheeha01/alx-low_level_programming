24a8f86faca15ad7b6a316bcc49d2dd62159d6d7
Tshegofatso Jan <mabatshegofatso842@gmail.com>
2023-02-18 16:35:19 -0800

    Putchar alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Alphabets
+ * using putchar()
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+}
