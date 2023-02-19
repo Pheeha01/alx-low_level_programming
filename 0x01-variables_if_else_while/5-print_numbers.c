1b11e350cb0e13eee65105886dd8d755ed8b8ab6
Tshegofatso Jan <mabatshegofatso842@gmail.com>
2023-02-18 17:28:37 -0800

    single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print ut digits
+ * base 10
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}
