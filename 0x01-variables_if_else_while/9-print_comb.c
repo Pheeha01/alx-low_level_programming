7b60e0f1bfc0ee52499152ad8338802a14011ad1
Tshegofatso Jan <mabatshegofatso842@gmail.com>
2023-02-19 15:53:54 -0800

    Double digits in ascending order

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
  * main - printing all possible combinations
- * using Putchar, need to study it
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 48; num < 58; num++)
+	for (n = 48; n < 58; n++)
 	{
-		putchar(num);
-		if (num != 57)
+		putchar(n);
+		if (n != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-	}
+	
+
 	putchar('\n');
 	return (0);
 }
