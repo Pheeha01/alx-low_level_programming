7b632ccc5db062f385dca398e3d3c78a3da0797a
Tshegofatso Jan <mabatshegofatso842@gmail.com>
2023-02-22 17:26:14 -0800

    lowercase detect

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- * main - lower case
- * putchar
- * Return: Always 0
+ * _islower - printing lower cases
+ *@c: c contains all chracters that are lower cases
+ *
+ * description: prints lower case characters and compares them to character
+ * Return: 1 if char is lowercase, otherwise 0
  */
 
 int _islower(int c)
